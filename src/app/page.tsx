2c88c1bf355d0ed33e663c4419a9f03be9876bd8
LepharamRamchiary <lepharamchiary@gmail.com>
2024-09-26 13:30:48 +0530

    add new prodect section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Hero from "@/components/Hero";
+import NewProtucts from "@/components/NewProtucts";
 
 export default function Home() {
   return <main>
     <Hero />
+    <NewProtucts />
   </main>;
 }
---
+++ src/components/NewProtucts.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import ProductCard from "./ProductCard";
+
+const productDatas = [
+  {
+    img: "/product1.jpg",
+    title: "Trending Item",
+    desc: "Women's Latest Fashion Sale",
+    rating: 3,
+    price: "200",
+  },
+  {
+    img: "/product2.jpg",
+    title: "Stylish Sneakers",
+    desc: "Comfortable and trendy footwear",
+    rating: 4,
+    price: "120",
+  },
+  {
+    img: "/product3.jpg",
+    title: "Classic Handbag",
+    desc: "Elegant design for every occasion",
+    rating: 5,
+    price: "250",
+  },
+  {
+    img: "/product4.jpg",
+    title: "Cool Sunglasses",
+    desc: "Protect your eyes in style",
+    rating: 4,
+    price: "80",
+  },
+  {
+    img: "/product5.jpg",
+    title: "Stylish Jacket",
+    desc: "Perfect for chilly evenings",
+    rating: 4,
+    price: "150",
+  },
+  {
+    img: "/product6.jpg",
+    title: "Elegant Dress",
+    desc: "For a night out or special occasions",
+    rating: 5,
+    price: "300",
+  },
+  {
+    img: "/product7.jpg",
+    title: "Trendy Backpack",
+    desc: "Fashionable and functional for daily use",
+    rating: 4,
+    price: "90",
+  },
+  {
+    img: "/product8.jpg",
+    title: "Luxury Watch",
+    desc: "Timeless elegance and precision",
+    rating: 4,
+    price: "500",
+  },
+];
+
+const NewProtucts = () => {
+  return (
+    <div>
+      <div className="container pt-16">
+        <h2 className="font-medium text-2xl py-4">New Products</h2>
+        <div className="grid grid-cols-1 place-items-center sm:place-items-start sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 xl:gap-x-20 xl:gap-y-10">
+          {productDatas.map((item, index) => (
+            <ProductCard
+              key={index}
+              img={item.img}
+              title={item.title}
+              desc={item.desc}
+              rating={item.rating}
+              price={item.price}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewProtucts;
