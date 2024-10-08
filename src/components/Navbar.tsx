3c7b600e3bbe55c76e7b557fa698e3a2dd416511
LepharamRamchiary <lepharamchiary@gmail.com>
2024-10-08 10:41:00 +0530

    add Jewelry product card image are demo.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,8 +8,8 @@ const Navbar = () => {
         <div className="flex w-fit gap-10 mx-auto font-medium py-4 text-blackish">
           <Link href="/" className="navbar__link relative uppercase">HOMe</Link>
           <Link href="/mens" className="navbar__link relative uppercase">men's</Link>
-          <Link href="womens" className="navbar__link relative uppercase">women's</Link>
-          <Link href="#" className="navbar__link relative uppercase">jewelry</Link>
+          <Link href="/womens" className="navbar__link relative uppercase">women's</Link>
+          <Link href="/jewelry" className="navbar__link relative uppercase">jewelry</Link>
           <Link href="#" className="navbar__link relative uppercase">perfume</Link>
           <Link href="#" className="navbar__link relative uppercase">blog</Link>
           <Link href="#" className="navbar__link relative uppercase">hot offers</Link>
